a65bd1adb2f0123a442d216216ca2ee77ac7f029
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-19 01:36:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const st = document.querySelectorAll("strong")
+
 function highlight() {
     //Write your code here
-
-
+	st.forEach(el =>{
+		el.addEventListener("mouseover",()=>{
+			el.style.color = "blue"
+		})
+	})
 }
-
-
 function return_normal() {
     //Write your code here
-
-    
+	st.forEach(el =>{
+		el.addEventListener("mouseout",()=>{
+			el.style.color = "black"
+		})
+	})
 }
