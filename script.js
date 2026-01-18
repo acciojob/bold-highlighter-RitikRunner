c1aee2dbb1772207f23e3c981ee12c0f38b67266
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-19 02:18:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const st = document.querySelectorAll("strong")
+const link = document.getElementById("link")
 
 function highlight() {
     //Write your code here
 	st.forEach(el =>{
-		el.addEventListener("mouseover",()=>{
-			el.style.color = "blue"
+	link.addEventListener("mouseover",()=>{
+	el.style.color = "blue"
 		})
 	})
 }
 function return_normal() {
     //Write your code here
 	st.forEach(el =>{
-		el.addEventListener("mouseout",()=>{
-			el.style.color = "black"
+	link.addEventListener("mouseout",()=>{
+	el.style.color = "black"
 		})
 	})
 }
